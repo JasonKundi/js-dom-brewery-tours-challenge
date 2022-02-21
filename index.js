68f8548301122278e5f6ae32a1639f681388c9bd
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-02-21 14:27:46 +0000

    push

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ function clear() {
 }
 
 function render() {
+  clear()
   listenToSelectStateForm();
   renderBreweryList();
 }
 
 const form1 = document.querySelector("#select-state-form");
 const formInputEl = document.querySelector("#select-state");
+const formInputValue = formInputEl.value;
 
 function listenToSelectStateForm() {
   form1.addEventListener("submit", function (event) {
     event.preventDefault();
-    const formInputValue = formInputEl.value;
     const url = "https://api.openbrewerydb.org/breweries?by_state=";
 
     fetch(url + formInputValue)
@@ -34,10 +35,49 @@ function listenToSelectStateForm() {
   });
 }
 
+const formFilter = document.querySelector("#filter-by-type-form")
+const filterInputValue = document.querySelector("#filter-by-type")
+
+function checkBreweryType(brewery) {
+  for (const brewery of state.breweries) {
+    if (brewery.brewery_type == "Micro") {
+      return true
+    }
+    if (brewery.brewery_type == "Regional") {
+      return true
+    }
+    if (brewery.brewery_type == "Brewpub") {
+      return true
+    }
+  }
+  
+}
+
+function listenToFilterByTypeForm() {
+  formFilter.addEventListener("change", function(event) {
+    event.preventDefault();
+
+    const filterInputValue = checkBreweryType(brewery);
+    const urlByType = `${formInputValue}&by_type=`;
+    
+    fetch(urlByType + filterInputValue)
+      .then(function (response) {
+        console.log("raw response", response);
+        return response.json();
+      })
+      .then(function (breweries) {
+        state.breweries = breweries;
+        console.log(breweries);
+      });
+    render();
+  });
+
+
+}
+
 const ulEl = document.querySelector("#breweries-list");
 
 function renderBreweryList() {
-  clear();
   for (const brewery of state.breweries) {
     const liEl = document.createElement("li");
     const h2El = document.createElement("h2");
@@ -69,12 +109,11 @@ function renderBreweryList() {
     const aTag = document.createElement("a");
     aTag.innerText = brewery.website_url;
 
-    liEl.append(h2El, divEl, sectionEl, sectionEl2, sectionEl3);
-    sectionEl.append(h3El, pEl, pEl2);
-    sectionEl2.append(h3El2, pEl3);
-    sectionEl3.append(aTag);
+    liEl.append(h2El, divEl, sectionEl, h3El, pEl, pEl2, sectionEl2, h3El2, pEl3, sectionEl3, aTag);
     ulEl.append(liEl);
+
   }
+  
 }
 
 render();
